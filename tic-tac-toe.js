7cb49dfab11fd9e5d38f99076324f8fc7c3d81ad
Von Harris <vonharris00@gmail.com>
2023-10-22 23:25:43 -0500

    Updated JavaScript Code

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-
   const squares = document.querySelectorAll('#board > div');
-
   squares.forEach(function (square) {
     square.classList.add('square');
   });
