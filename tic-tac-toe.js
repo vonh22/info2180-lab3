6cc1f965dd36914bdbef2c46823d43b88b5faee0
Von Harris <vonharris00@gmail.com>
2023-10-22 22:41:06 -0500

    updated javascript

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,72 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Select all square elements in the game board
-    const squares = document.querySelectorAll('#board > div');
-  
-    // Loop through the squares and add the 'square' class to each
-    squares.forEach(function (square) {
-      square.classList.add('square');
+
+  const squares = document.querySelectorAll('#board > div');
+
+  squares.forEach(function (square) {
+    square.classList.add('square');
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const squares = document.querySelectorAll('#board > div');
+  const status = document.getElementById('status');
+  const newGameButton = document.querySelector('.btn'); 
+  let currentPlayer = 'X';
+  const gameState = ["", "", "", "", "", "", "", "", ""];
+
+  function checkWinner(player) {
+    const winningCombinations = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+      [0, 4, 8], [2, 4, 6]            
+    ];
+
+    for (const combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (gameState[a] === player && gameState[b] === player && gameState[c] === player) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  squares.forEach(function (square, index) {
+    square.addEventListener('mouseenter', function () {
+      if (!square.classList.contains('X') && !square.classList.contains('O')) {
+        square.classList.add('hover');
+      }
+    });
+
+    square.addEventListener('mouseleave', function () {
+      square.classList.remove('hover');
+    });
+
+    square.addEventListener('click', function () {
+      if (!square.classList.contains('X') && !square.classList.contains('O')) {
+        square.classList.add(currentPlayer);
+        square.textContent = currentPlayer;
+        gameState[index] = currentPlayer;
+
+        if (checkWinner(currentPlayer)) {
+          status.textContent = `Congratulations! ${currentPlayer} is the Winner!`;
+          status.classList.add('you-won');
+        } else {
+          currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
+      }
     });
   });
+
+  
+  newGameButton.addEventListener('click', function () {
+    squares.forEach(function (square) {
+      square.textContent = '';
+      square.classList.remove('X', 'O', 'hover');
+    });
+    gameState.fill('');
+    status.textContent = 'Move your mouse over a square and click to play an X or an O.';
+    status.classList.remove('you-won');
+    currentPlayer = 'X'; 
+  });
+});
+
